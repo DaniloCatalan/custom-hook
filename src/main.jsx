c5d2802575b891fb04d2d2b679d77eee8ce7ff26
Danilo <danilo.e.catalan@gmail.com>
2022-07-12 16:03:17 -0400

    add hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
+import { Padre } from "./07-tarea-memo/Padre";
 
+// import { CallbackHook } from "./06-memos/CallbackHook";
+// import { MemoHook } from "./06-memos/MemoHook";
+// import { Memorize } from "./06-memos/Memorize";
+// import { Layout } from "./05-useLayoutEffect/Layout";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { MultipleCustomHooks } from "./03-example/MultipleCustomHooks";
+// import { FormWithCustomHook } from "./02-useEffect/FormWithCustomHook";
 // import { SimpleForm } from "./02-useEffect/SimpleForm";
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp'
@@ -15,6 +22,13 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     {/* <CounterApp/> */}
     {/* <CounterWithCustomHook/> */}
     {/* <SimpleForm/> */}
-    <FormWithCustomHook/>
+    {/* <FormWithCustomHook/> */}
+    {/* <MultipleCustomHooks/> */}
+    {/* <FocusScreen/> */}
+    {/* <Layout/> */}
+    {/* <Memorize /> */}
+    {/* <MemoHook/> */}
+    {/* <CallbackHook/> */}
+    <Padre/>
   </React.StrictMode>
 );
